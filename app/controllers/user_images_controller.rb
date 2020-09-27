2c33fe05af2cdd9a1ef74dfb1aa2708702a448de
alerner1 <alyssplerner@gmail.com>
2020-09-27 14:11:11 -0400

    user_image controller

--- app/controllers/user_images_controller.rb
+++ app/controllers/user_images_controller.rb
@@ -3,4 +3,16 @@ class UserImagesController < ApplicationController
     user_images = UserImage.all
     render json: user_images
   end
+
+  def create
+    user_image = UserImage.find_or_create_by(image_id: params[:user_image][:image_id], user_id: params[:user_image][:user_id])
+    render json: user_image
+  end
+
+  private
+
+  def user_image_params
+    params.require(:user_image).permit(:user_id, :image_id, :moves, :completed)
+  end
+  
 end
